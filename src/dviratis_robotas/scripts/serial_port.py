87678aadf50bfef9fa6ed6f2d51b7625b9f79449
Mantas A <mantas.ambraziunas@gmail.com>
2020-12-02 16:28:24 +0200

    zinute AccGyro ir node gauti tiem duomenim

--- src/dviratis_robotas/scripts/serial_port.py
+++ src/dviratis_robotas/scripts/serial_port.py
@@ -1,34 +1,55 @@
 #!/usr/bin/env python3
 
 import rospy
-from dviratis_robotas.msg import USB_message
+from dviratis_robotas.msg import AccGyr_msg
 import serial
 import sys
+from numpy import int16
+
+# settings
+accFS = 8  # 2 4 8 16
+gyroFS = 1000   # 250 500 1000 2000
 
 
 def talker():
     port = '/dev/ttyACM0'
+    acc_values = [0]*3
+    gyr_values = [0]*3
 
     args = rospy.myargv(argv=sys.argv)
-    # with rospy.myargv(argv=sys.argv) as args:
     if len(args) == 2:
         port = args[1]
 
     serial_port = serial.Serial(port, timeout=0)
     serial_port.flush
 
-    pub = rospy.Publisher('USB_message', USB_message, queue_size=10)
+    pub = rospy.Publisher('AccGyr_msg', AccGyr_msg, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10)  # 10hz
 
-    dataToPublish = USB_message()
+    dataToPublish = AccGyr_msg()
 
     while not rospy.is_shutdown():
+        serial_port.write(b'!')
+        rospy.sleep(0.001)
+
         if serial_port.in_waiting > 0:
-            dataToPublish.array = serial_port.read_all()
-            rospy.loginfo('Published %s bytes> "%s"' %
-                          (len(dataToPublish.array), dataToPublish.array))
-            pub.publish(dataToPublish)
+            array = serial_port.read_all()
+            if(len(array) == 14):
+                for i in range(3):
+                    acc_values[i] = int16(((array[2*i] & 0xff) << 8) | (array[2*i+1] & 0xff)) * \
+                        accFS / 32768 * 9.8
+                    gyr_values[i] = int16(((array[2*i+8] & 0xff) << 8) | (array[2*i+9] & 0xff)) * \
+                        gyroFS / 32768
+
+                dataToPublish.acc = acc_values
+                dataToPublish.gyro = gyr_values
+
+                rospy.loginfo('acc: {:04.4f} {:04.4f} {:04.4f} gyro: {:04.4f} {:04.4f} {:04.4f}'.format(
+                    acc_values[0], acc_values[1], acc_values[2], gyr_values[0], gyr_values[1], gyr_values[2]))
+
+                # publish message
+                pub.publish(dataToPublish)
         rate.sleep()
 
 
